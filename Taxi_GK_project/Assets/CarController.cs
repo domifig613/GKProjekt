aa125e19e5ed7c035790bd3410b7073177f8df19
domifig613 <32535456+domifig613@users.noreply.github.com>
2020-03-21 10:03:05 +0100

    first part of car move

---
+++ Taxi_GK_project/Assets/CarController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] private float acceleration = 5f;
+    [SerializeField] private float maxSpeed = 10f;
+    [SerializeField] private float turningSpeed = 45f;
+    [SerializeField] private AnimationCurve turningFactorBySpeed;
+    [SerializeField] private Rigidbody rigidbody;
+
+    private float currentSpeed = 0f;
+
+    private float verticalInput = 0;
+    private float horizontalInput = 0;
+
+   
+    public void Accelerate()
+    {
+        //   Debug.Log(rigidbody.velocity.magnitude);
+
+        if (currentSpeed < maxSpeed)
+        {
+            rigidbody.AddForce(rigidbody.transform.forward * acceleration);
+        }
+    }
+
+    private void Update()
+    {
+        currentSpeed = rigidbody.velocity.magnitude;
+    }
+
+    public void Break()
+    {
+        if (currentSpeed < maxSpeed)
+        {
+            rigidbody.AddForce(-rigidbody.transform.forward * acceleration);
+        }
+    }
+
+    public void ChangeDirection(bool left)
+    {
+        float direction = left ? -1 : 1;
+        
+     //   if (currentSpeed > 0.1 && rigidbody.angularVelocity.magnitude < 3)
+        {
+            Debug.Log(Vector3.up * turningSpeed * direction);
+            rigidbody.AddTorque(Vector3.up * turningSpeed * direction, ForceMode.Force);
+        }
+
+      //  Debug.Log(rigidbody.angularVelocity.magnitude);
+    }
+}
---
+++ Taxi_GK_project/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private CarController carController;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            carController.ChangeDirection(true);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            carController.ChangeDirection(false);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            carController.Accelerate();
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            carController.Break();
+        }
+    }
+}
