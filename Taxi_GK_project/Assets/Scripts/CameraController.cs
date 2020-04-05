04d864454541cd9c8b79ba5baf278c693773db1b
domifig613 <32535456+domifig613@users.noreply.github.com>
2020-04-05 21:33:57 +0200

    Add assets and WIP with 1/10 part of city

---
+++ Taxi_GK_project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private float cameraSpeed;
+
+    private Camera camera;
+    private Vector3 distanceFromTarget;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+        distanceFromTarget = camera.transform.position - targetTransform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        camera.transform.position = Vector3.Lerp(camera.transform.position, targetTransform.position + distanceFromTarget, Time.deltaTime * cameraSpeed);
+    }
+}
---
+++ Taxi_GK_project/Assets/Scripts/WheelOnTriggerCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelOnTriggerCollider : MonoBehaviour
+{
+    public bool wheelOnStreet { private set; get; }
+    
+    private int collisionCount = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collisionCount++;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        collisionCount--;
+    }
+
+    private void Update()
+    {
+        wheelOnStreet = collisionCount > 0;
+    }
+}
