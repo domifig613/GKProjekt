7723627577d8f50a87fd1d7369bf10a6c8924e87
domifig613 <32535456+domifig613@users.noreply.github.com>
2020-05-10 23:21:11 +0200

    Add building controller in city biom

---
+++ Taxi_GK_project/Assets/Scripts/CityBuildingsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityBuildingsController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> buildingsTransform;
+    [SerializeField] private Transform playerCarTransform;
+    [SerializeField] private int refreshRate;
+
+    private void Start()
+    {
+        StartCoroutine(ChangeBuildingsVisibleStateCor());
+    }
+
+    private IEnumerator ChangeBuildingsVisibleStateCor()
+    {
+        while(true)
+        {
+            foreach (var building in buildingsTransform)
+            {
+                if(building.transform.position.z > playerCarTransform.position.z)
+                {
+                    building.SetActive(false);
+                }
+                else
+                {
+                    building.SetActive(true);
+                }
+            }
+
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
