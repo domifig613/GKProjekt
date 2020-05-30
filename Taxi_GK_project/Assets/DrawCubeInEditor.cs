b1eb63fb34f1d9bf489f35c11fbb023a9741d4d9
Patryk Domin <PatrykDomin@github.com>
2020-05-30 18:56:17 +0200

    Add gizmos draw cube script and collision walls in desert biom

---
+++ Taxi_GK_project/Assets/DrawCubeInEditor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DrawCubeInEditor : MonoBehaviour
+{
+    [ExecuteInEditMode]
+    void OnDrawGizmos()
+    {
+        // Draw a semitransparent blue cube at the transforms position
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+}
