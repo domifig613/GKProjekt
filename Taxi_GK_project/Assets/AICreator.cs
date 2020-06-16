9a54dc3125006eb160587a1d5f557258b3465e30
Patryk Domin <PatrykDomin@github.com>
2020-06-17 00:02:35 +0200

    Add more code for ai

---
+++ Taxi_GK_project/Assets/AICreator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICreator : MonoBehaviour
+{
+    [SerializeField] private List<Route> routes;
+    [SerializeField] private List<GameObject> carsPrefabs;
+    [SerializeField] private GameObject carControllerPrefab;
+    [SerializeField] private List<int> routesIndexCounts;
+    [SerializeField] private GameObject controllerParent;
+    [SerializeField] private GameObject carParent;
+     
+    private void Awake()
+    {
+        for (int i = 0; i < routes.Count; i++)
+        {
+            List<int> occupiedIndexes = new List<int>();
+            int maxIndex = routes[i].PointsLenght;
+
+            for (int j = 0; j < routesIndexCounts[i]; j++)
+            {
+                int index;
+
+                do
+                {
+                    index = Random.Range(0, maxIndex);
+                } 
+                while (occupiedIndexes.Contains(index));
+
+                occupiedIndexes.Add(index);
+
+                GameObject car = Instantiate(carsPrefabs[Random.Range(0, carsPrefabs.Count)]);
+                car.transform.SetParent(carParent.transform);
+
+                GameObject carController = Instantiate(carControllerPrefab);
+                carController.GetComponent<AIController>().Init(routes[i], car.GetComponent<AIInputCarController>(), index);
+                carController.transform.SetParent(controllerParent.transform);
+            }
+        }
+    }
+}
